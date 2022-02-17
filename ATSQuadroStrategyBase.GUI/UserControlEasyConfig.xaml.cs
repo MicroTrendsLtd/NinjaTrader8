9bd8ce7c6750b70fbc877ef985c3a5c5b04bfc07
Trader Leeson <trader@algotradingsystems.net>
2022-02-17 11:00:57 -0600

    ATSQuadroStrategyBase.GUI version 2022.1.1.7.3   .net 4.8

---
+++ ATSQuadroStrategyBase.GUI/UserControlEasyConfig.xaml.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.WpfPropertyGrid;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ATSQuadroStrategyBase.GUI
+{
+    /// <summary>
+    /// Interaction logic for UserControlEasyConfig.xaml
+    /// </summary>
+    public partial class UserControlEasyConfig : UserControl
+    {
+        //routedEvents
+        public event RoutedEventHandler OnLoadClick;
+        public event RoutedEventHandler OnRefreshClick;
+        public event RoutedEventHandler OnSaveClick;
+        public event RoutedEventHandler OnSaveAsClick;
+        public event RoutedEventHandler OnOkClick;
+        public event RoutedEventHandler OnCancelClick;
+        //public event RoutedEventHandler OnCloseClick;// use ok
+        public event RoutedEventHandler OnRestartClick;
+        public event RoutedEventHandler OnApplyClick;
+
+        public UserControlEasyConfig()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void BtnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            OnLoadClick?.Invoke(this, e);
+        }
+
+        private void BtnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            OnRefreshClick?.Invoke(this, e);
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            OnSaveClick?.Invoke(this, e);
+        }
+
+        private void BtnSaveAs_Click(object sender, RoutedEventArgs e)
+        {
+            OnSaveAsClick?.Invoke(this, e);
+        }
+
+        private void BtnOk_Click(object sender, RoutedEventArgs e)
+        {
+            OnOkClick?.Invoke(this, e);
+        }
+
+        private void BtnApply_Click(object sender, RoutedEventArgs e)
+        {
+            OnApplyClick?.Invoke(this, e);
+
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            OnCancelClick?.Invoke(this, e);
+        }
+
+        private void BtnRestart_Click(object sender, RoutedEventArgs e)
+        {
+            OnRestartClick?.Invoke(this, e);
+        }
+
+
+        #region Properties
+        public object PropertyGridSetupSelectedObject
+        {
+            set
+            {
+                PropertyGrid1.SelectedObject = value;
+            }
+        }
+
+        public object PropertyGridFiltersSelectedObject
+        {
+            set
+            {
+                PropertyGrid2.SelectedObject = value;
+            }
+        }
+
+        public object PropertyGridExitsSelectedObject
+        {
+            set
+            {
+                PropertyGrid3.SelectedObject = value;
+            }
+        }
+
+        public object PropertyGridHTFSelectedObject
+        {
+            set
+            {
+                PropertyGrid4.SelectedObject = value;
+            }
+        }
+
+        public object PropertyGridTradeManSelectedObject
+        {
+            set
+            {
+                PropertyGrid5.SelectedObject = value;
+            }
+        }
+
+        public PropertyGrid PropertyGridSetup
+        {
+            get
+            {
+                return PropertyGrid1;
+            }
+        }
+
+        public PropertyGrid PropertyGridFilters
+        {
+            get
+            {
+                return PropertyGrid2;
+            }
+        }
+
+        public PropertyGrid PropertyGridExits
+        {
+            get
+            {
+                return PropertyGrid3;
+            }
+        }
+
+        public PropertyGrid PropertyGridHTF
+        {
+            get
+            {
+                return PropertyGrid4;
+            }
+        }
+
+        public PropertyGrid PropertyGridTradeMan
+        {
+            get
+            {
+                return PropertyGrid5;
+            }
+        }
+        #endregion
+    }
+}
---
+++ ATSQuadroStrategyBase.GUI/UserControlNinjaBuddy.xaml.cs
@@ -0,0 +1,227 @@
+﻿//using BWTNT8Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ATSQuadroStrategyBase.GUI
+{
+    /// <summary>
+    /// Interaction logic for UserControlNinjaBuddy.xaml
+    /// </summary>
+    public partial class UserControlNinjaBuddy : UserControl
+    {
+
+        public event RoutedEventHandler OnAutoClick;
+        public event RoutedEventHandler OnAutoLongClick;
+        public event RoutedEventHandler OnAutoShortClick;
+
+        public event RoutedEventHandler OnBuyClick;
+        public event RoutedEventHandler OnSellClick;
+        public event RoutedEventHandler OnOCOBreakoutClick;
+        public event RoutedEventHandler OnBoxUpperClick;
+        public event RoutedEventHandler OnBoxLowerClick;
+        public event RoutedEventHandler OnBoxClearClick;
+
+        public event RoutedEventHandler OnCloseClick;
+        public event RoutedEventHandler OnTrail50Click;
+        public event RoutedEventHandler OnTrailHiLoClick;
+        public event RoutedEventHandler OnBreakEvenClick;
+        public event RoutedEventHandler OnTrailTriggerClick;
+        public event RoutedEventHandler OnTrail1Click;
+        public event RoutedEventHandler OnTrail2Click;
+        public event RoutedEventHandler OnTrail3Click;
+        public event RoutedEventHandler OnTrail4Click;
+
+        private Brush shortBGColor, longBGColor, shortTextColor, longTextColor;
+
+        public UserControlNinjaBuddy()
+        {
+            InitializeComponent();
+
+            try 
+            {
+                shortTextColor = Brushes.Red;
+                longTextColor = Brushes.LimeGreen;
+
+                shortBGColor = (Brush)new BrushConverter().ConvertFrom("#FFB70000");
+                longBGColor = (Brush)new BrushConverter().ConvertFrom("#FF00D409");
+                shortTextColor = (Brush)new BrushConverter().ConvertFrom("#FFFF0000");
+                longTextColor = (Brush)new BrushConverter().ConvertFrom("#FF32CD32");
+
+
+            }
+            catch(Exception ex)
+            {
+                Debug.Print(ex.ToString());
+            }
+
+        }
+
+        private void textPositionState_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (textPositionState.Text == "1")
+            {
+                this.textBoxInstrumentFullName.Background = longBGColor;
+                this.textBoxInstrumentFullName.Foreground = Brushes.Black;
+                this.textBoxHeader.Foreground = longTextColor;
+            }
+            else if (textPositionState.Text == "-1")
+            {
+                this.textBoxInstrumentFullName.Background = shortBGColor;
+                this.textBoxInstrumentFullName.Foreground = Brushes.White;
+                this.textBoxHeader.Foreground = shortTextColor;
+            }
+            else
+            {
+                this.textBoxInstrumentFullName.Background = Brushes.Black;
+                this.textBoxInstrumentFullName.Foreground = Brushes.White;
+                this.textBoxHeader.Foreground = Brushes.White;
+            }
+
+        }
+
+        private void textUnrealizedPL_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            if (textPositionState.Text == "0")
+            {
+                this.textBoxHeaderPL.Foreground = Brushes.Black;
+            }
+            else  if (textUnrealizedPL.Text.Contains("-"))
+            {
+                this.textBoxHeaderPL.Foreground = shortTextColor;
+            }
+           else
+            {
+                this.textBoxHeaderPL.Foreground = longTextColor;
+            }
+
+        }
+        
+
+        private void cbAutomatedTrading_Checked(object sender, RoutedEventArgs e)
+        {
+            OnAutoClick?.Invoke(this, e);
+        }
+
+        private void cbAutomatedTrading_Unchecked(object sender, RoutedEventArgs e)
+        {
+            OnAutoClick?.Invoke(this, e);
+        }
+
+        private void cbAutoLong_Checked(object sender, RoutedEventArgs e)
+        {
+            OnAutoLongClick?.Invoke(this, e);
+        }
+
+        private void cbAutoLong_Unchecked(object sender, RoutedEventArgs e)
+        {
+            OnAutoLongClick?.Invoke(this, e);
+        }
+
+        private void cbAutoShort_Unchecked(object sender, RoutedEventArgs e)
+        {
+            OnAutoShortClick?.Invoke(this, e);
+        }
+
+        private void cbAutoShort_Checked(object sender, RoutedEventArgs e)
+        {
+            OnAutoShortClick?.Invoke(this, e);
+        }
+
+        private void btnBoxClear_Click(object sender, RoutedEventArgs e)
+        {
+            OnBoxClearClick?.Invoke(this, e);
+        }
+
+        private void btnBuy_Click(object sender, RoutedEventArgs e)
+        {
+            OnBuyClick?.Invoke(this, e);
+        }
+
+        private void btnSell_Click(object sender, RoutedEventArgs e)
+        {
+            OnSellClick?.Invoke(this, e);
+        }
+
+        private void btnOCOBreakout_Click(object sender, RoutedEventArgs e)
+        {
+            OnOCOBreakoutClick?.Invoke(this, e);
+        }
+
+        private void cbUpper_Checked(object sender, RoutedEventArgs e)
+        {
+            OnBoxUpperClick?.Invoke(this, e);
+        }
+
+        private void cbLower_Checked(object sender, RoutedEventArgs e)
+        {
+            OnBoxLowerClick?.Invoke(this, e);
+        }
+
+        private void btnClosePositions_Click(object sender, RoutedEventArgs e)
+        {
+            OnCloseClick?.Invoke(this, e);
+            
+        }
+
+        private void btnTrail50_Click(object sender, RoutedEventArgs e)
+        {
+            OnTrail50Click?.Invoke(this, e);
+        }
+
+        private void btnTrailHiLo_Click(object sender, RoutedEventArgs e)
+        {
+            OnTrailHiLoClick?.Invoke(this, e);
+        }
+
+        private void btnBreakEven_Click(object sender, RoutedEventArgs e)
+        {
+            OnBreakEvenClick?.Invoke(this, e);
+        }
+
+        private void btnSetTrail_Click(object sender, RoutedEventArgs e)
+        {
+            OnTrailTriggerClick?.Invoke(this, e);
+        }
+
+        private void btnTrail1_Click(object sender, RoutedEventArgs e)
+        {
+            OnTrail1Click?.Invoke(this, e);
+        }
+
+        private void btnTrail2_Click(object sender, RoutedEventArgs e)
+        {
+            OnTrail2Click?.Invoke(this, e);
+        }
+
+        private void btnTrail3_Click(object sender, RoutedEventArgs e)
+        {
+            OnTrail3Click?.Invoke(this, e);
+        }
+
+        private void btnTrail4_Click(object sender, RoutedEventArgs e)
+        {
+            OnTrail4Click?.Invoke(this, e);
+        }
+
+        private void HeaderSite_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+       
+    }
+}
